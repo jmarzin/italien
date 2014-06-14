560809812efefa0b618e78115d6cfd5bbc6bf1ce
Jacques Marzin​ <jacques.marzin@free.fr>
2014-06-14 17:02:08 +0200

    lundi

--- app/models/parametre.rb
+++ app/models/parametre.rb
@@ -1,3 +1,18 @@
+class String
+  def to_range
+    case self.count('.')
+      when 2
+        elements = self.split('..')
+        return Range.new(elements[0].to_i, elements[1].to_i)
+      when 3
+        elements = self.split('...')
+        return Range.new(elements[0].to_i, elements[1].to_i-1)
+      else
+        raise ArgumentError.new("Couldn't convert to Range: #{str}")
+    end
+  end
+end
+
 class ParametreValidator < ActiveModel::Validator
   def validate(record)
 
