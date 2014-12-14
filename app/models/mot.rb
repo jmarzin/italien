04f867f23228540a88b3b8be5df43c8da468151c
Jacques Marzin​ <jacques.marzin@free.fr>
2014-12-14 18:37:19 +0100

    dimanche 13/12/2014

--- app/models/mot.rb
+++ app/models/mot.rb
@@ -62,7 +62,7 @@ class Mot < ActiveRecord::Base
     liste
   end
 
-  def self.api_v2
+  def self.api_v2 
     liste = []
     Mot.order(:category_id, :mot_directeur, :francais).each do |mot|
       liste << [mot.id, mot.category_id, mot.francais, mot.mot_directeur, mot.italien]
